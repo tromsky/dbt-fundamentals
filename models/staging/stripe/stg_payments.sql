53ffaf5b6fb0a55fd328ae23239a1400595576e8
Tom Ross <tromsky@gmail.com>
2023-09-26 22:02:14 +0000

    Add cents to dollars macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -8,7 +8,7 @@ with
             status,
 
             -- amount is stored in cents, convert it to dollars
-            amount / 100 as amount,
+            {{ cents_to_dollars('amount') }} as amount,
             created as created_at
 
         from source
